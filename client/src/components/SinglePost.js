4dfd51caadf624af3106b1a1acb650f847626eea
DenimCity <altidorj1@gmail.com>
2018-02-14 18:21:35 -0500

    edited single post formatting

--- client/src/components/SinglePost.js
+++ client/src/components/SinglePost.js
@@ -13,8 +13,8 @@ const SinglePost = props => {
         <img width="200" src={props.post_photo} alt="" />
       </div>
       <div>
-        <div>{props.title}</div>
-        <div>{props.body}</div>
+        <h3>{props.title}</h3>
+        <p>{props.body}</p>
       </div>
       <button type="submit" onClick={handleClick}>
         Delete
