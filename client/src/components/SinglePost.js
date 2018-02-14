2f5cf5d5d859e8f4068cb7c832ae0789d7918397
DenimCity <altidorj1@gmail.com>
2018-02-14 18:02:26 -0500

    refactored single post view

--- client/src/components/SinglePost.js
+++ client/src/components/SinglePost.js
@@ -1,22 +1,27 @@
-import React from 'react'
-
-const SinglePost = (props) => {
-
-    const handleClick = () => {
-        if (window.confirm(`Are you sure you want to delete ${props.title}?`)) {
-            props.deletePost(props.post.id)
-        }
+import React from "react";
+import styled from "styled-components";
+const SinglePost = props => {
+  const handleClick = () => {
+    if (window.confirm(`Are you sure you want to delete ${props.title}?`)) {
+      props.deletePost(props.post.id);
     }
-    
-    return(
-        <div >
-            <img width="200" src={props.post_photo} alt="" />
-            <div>{props.title}</div>
-            <div>{props.body}</div>
-            <button type="submit" onClick={handleClick}>Delete</button>
-        </div>
-    )
-    
-}
+  };
 
-export default SinglePost
+  return (
+    <div>
+      <div>
+        <img width="200" src={props.post_photo} alt="" />
+      </div>
+      <div>
+        <div>{props.title}</div>
+        <div>{props.body}</div>
+      </div>
+      <button type="submit" onClick={handleClick}>
+        Delete
+      </button>
+      
+    </div>
+  );
+};
+
+export default SinglePost;
