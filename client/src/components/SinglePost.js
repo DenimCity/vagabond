05b3f3a3e88d5fe873ff7ee0439cc7cf857ce7f7
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-02-15 11:28:44 -0500

    minor changes

--- client/src/components/SinglePost.js
+++ client/src/components/SinglePost.js
@@ -1,28 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 const SinglePost = props => {
-  const handleClick = () => {
-    if (window.confirm(`Are you sure you want to delete ${props.title}?`)) {
-      props.deletePost(props.post.id);
-    }
-  };
+    const handleClick = () => {
+        if (window.confirm(`Are you sure you want to delete ${props.title}?`)) {
+            props.deletePost(props.post.id);
+        }
+    };
 
-  return (
-    <div>
-      <div>
-        <img width="200" src={props.post_photo} alt="" />
-      </div>
-      <div>
+    return (
+        <div>
+            <div>
+                <img width="200" src={props.post_photo} alt="" />
+            </div>
+            <div>
 
-        <h3>{props.title}</h3>
-        <p>{props.body}</p>
-      </div>
-      <button type="submit" onClick={handleClick}>
-        Delete
-      </button>
-      
-    </div>
-  );
+                <h3>{props.title}</h3>
+                <p>{props.body}</p>
+            </div>
+            <button type="submit" onClick={handleClick}>
+                Delete
+        </button>
+
+        </div>
+    );
 };
 
 export default SinglePost;
